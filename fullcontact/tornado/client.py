4233b3d08fb86439ab9bef648180222edb34519d
Karlis Lauva <k@karlis.me>
2016-06-17 10:15:43 -0600

    client: get rid of base http client

--- fullcontact/tornado/client.py
+++ fullcontact/tornado/client.py
@@ -3,10 +3,43 @@ import urllib
 
 from tornado import gen, httpclient
 
-from fullcontact.tornado import HTTPClient
 
+class CabApiClient(object):
 
-class CabApiClient(HTTPClient):
+    _MULTIPART_BOUNDARY = "1234cabapiclientboundary1234"
+
+    def _encode_multipart(self, parts):
+        """Encode a multipart request payload."""
+
+        lines = ""
+        for k, v in parts.items():
+
+            lines += "--%s\n" % self._MULTIPART_BOUNDARY
+            lines += "Content-Disposition: "
+            lines += 'form-data; name="%s"; filename="%s"\r\n' % (k, k)
+            if isinstance(v, dict):
+                lines += "Content-Type: application/json\r\n\r\n"
+                lines += '%s\r\n' % (json.dumps(v))
+            else:
+                lines += "Content-Type: image/jpeg\r\n\r\n"
+                lines += v
+                lines += "\r\n"
+        lines += "--%s--" % self._MULTIPART_BOUNDARY
+        return lines, "multipart/form-data; boundary=%s" % self._MULTIPART_BOUNDARY
+
+    def fetch(self, request):
+        client = httpclient.AsyncHTTPClient()
+        return client.fetch(request)
+
+    def body(self, res):
+        """
+            Returns decoded response body.
+        """
+
+        if hasattr(res, "body"):
+            return json.loads(res.body)
+        else:
+            return json.loads(res.response.body)
 
     @gen.coroutine
     def req(self, resource, body, token=None, headers=None, method=None):
